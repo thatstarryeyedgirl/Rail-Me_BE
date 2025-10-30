686d466a9a9043ac17f1c4de027f040c6402a25d
Valerie <okorovaleriechikanyima@gmail.com>
2025-10-30 11:00:13 +0100

    Updated Rail-Me project to enable deployment to render

--- accounts_app/serializers.py
+++ accounts_app/serializers.py
@@ -29,11 +29,11 @@ class PassengerRegistrationSerializer(serializers.ModelSerializer):
     def validate(self, attrs):
         errors = {}
         phone = attrs.get('phone_number')
-        password = attrs.get('password')
         
         if not phone.isdigit() or len(phone) < 11:
-            errors['phone_number'] = "Phone number must be at least 10 digits."
-        
+            errors['phone_number'] = "Phone number must be at least 11 digits."
+            
+        password = attrs.get('password')
         if len(password) < 12:
             errors['password'] = "Password must be at least 12 characters long."
         if errors:
